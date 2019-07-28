3cc476c487b077491dbc815378d1f285972d2be1
Cairo Ap. Campos <cairoapcampos@hotmail.com>
2019-07-28 18:15:29 -0300

    Update SecurityDog.sh

--- SecurityDog.sh
+++ SecurityDog.sh
@@ -489,6 +489,63 @@ else
 fi
 }
 
+EditFstab(){
+
+cp /etc/fstab /etc/fstab.old
+
+boot="defaults,nosuid 0 2"
+bar="defaults 0 1"
+home="defaults,nosuid,noexec,nodev 0 2"
+tmp="defaults,nosuid,noexec,nodev 0 2"
+var="defaults,nosuid,noexec,nodev 0 2"
+var_log="defaults,nosuid,noexec,noatime,nodev 0 2"
+
+cat /etc/fstab | grep -v "#" | grep -v swap | grep UUID > clFstab.txt
+
+while read line
+do
+a=$(echo "$line" | awk '{print $1}')
+b=$(echo "$line" | awk '{print $2}')
+c=$(echo "$line" | awk '{print $3}')
+   if [ $b = "/boot" ]
+   then
+   b="\/boot"
+   new_boot=$(echo "$a $b $c $boot")
+   sed -i 's/'"$a.*"'/'"$new_boot"'/' /etc/fstab
+   elif [ $b = "/" ]
+   then
+   b="\/"
+   new_bar=$(echo "$a $b $c $bar")
+   sed -i 's/'"$a.*"'/'"$new_bar"'/' /etc/fstab
+   elif [ $b = "/home" ]
+   then
+   b="\/home"
+   new_home=$(echo "$a $b $c $home")
+   sed -i 's/'"$a.*"'/'"$new_home"'/' /etc/fstab
+   elif [ $b = "/tmp" ]
+   then
+   b="\/tmp"
+   new_tmp=$(echo "$a $b $c $tmp")
+   sed -i 's/'"$a.*"'/'"$new_tmp"'/' /etc/fstab
+   elif [ $b = "/var" ]
+   then
+   b="\/var"
+   new_var=$(echo "$a $b $c $var")
+   sed -i 's/'"$a.*"'/'"$new_var"'/' /etc/fstab
+   elif [ $b = "/var/log" ]
+   then
+   b="\/var\/log"
+   new_var_log=$(echo "$a $b $c $var_log")
+   sed -i 's/'"$a.*"'/'"$new_var_log"'/' /etc/fstab
+   else
+   echo "Partição Desconhecida!"
+   fi
+done < clFstab.txt
+
+echo "Reinicie a máquina para que o arquivo /etc/fstab seja recarregado!"
+
+}
+
 ###########################
 ### Funções especificas ###
 ###########################
@@ -646,63 +703,6 @@ crontab jobs.txt
 rm jobs.txt
 }
 
-EditFstab(){
-
-cp /etc/fstab /etc/fstab.old
-
-boot="defaults,nosuid 0 2"
-bar="defaults 0 1"
-home="defaults,nosuid,noexec,nodev 0 2"
-tmp="defaults,nosuid,noexec,nodev 0 2"
-var="defaults,nosuid,noexec,nodev 0 2"
-var_log="defaults,nosuid,noexec,noatime,nodev 0 2"
-
-cat /etc/fstab | grep -v "#" | grep -v swap | grep UUID > clFstab.txt
-
-while read line
-do
-a=$(echo "$line" | awk '{print $1}')
-b=$(echo "$line" | awk '{print $2}')
-c=$(echo "$line" | awk '{print $3}')
-   if [ $b = "/boot" ]
-   then
-   b="\/boot"
-   new_boot=$(echo "$a $b $c $boot")
-   sed -i 's/'"$a.*"'/'"$new_boot"'/' /etc/fstab
-   elif [ $b = "/" ]
-   then
-   b="\/"
-   new_bar=$(echo "$a $b $c $bar")
-   sed -i 's/'"$a.*"'/'"$new_bar"'/' /etc/fstab
-   elif [ $b = "/home" ]
-   then
-   b="\/home"
-   new_home=$(echo "$a $b $c $home")
-   sed -i 's/'"$a.*"'/'"$new_home"'/' /etc/fstab
-   elif [ $b = "/tmp" ]
-   then
-   b="\/tmp"
-   new_tmp=$(echo "$a $b $c $tmp")
-   sed -i 's/'"$a.*"'/'"$new_tmp"'/' /etc/fstab
-   elif [ $b = "/var" ]
-   then
-   b="\/var"
-   new_var=$(echo "$a $b $c $var")
-   sed -i 's/'"$a.*"'/'"$new_var"'/' /etc/fstab
-   elif [ $b = "/var/log" ]
-   then
-   b="\/var\/log"
-   new_var_log=$(echo "$a $b $c $var_log")
-   sed -i 's/'"$a.*"'/'"$new_var_log"'/' /etc/fstab
-   else
-   echo "Partição Desconhecida!"
-   fi
-done < clFstab.txt
-
-echo "Reinicie a máquina para que o arquivo /etc/fstab seja recarregado!"
-
-}
-
 #######################################
 ### Fuções com retorno para o Menu  ###
 #######################################
@@ -777,6 +777,11 @@ RtMenu
 
 RmPKGRT() {
 RmPKG
+RtMenu
+}
+
+RtIDRT() {
+RtID
 RtMenu
 }
 
