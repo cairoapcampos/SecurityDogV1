c8c172c42e01bbfff3961ef715f5d708bd7de543
Cairo Ap. Campos <cairoapcampos@hotmail.com>
2019-08-01 21:05:05 -0300

    Update SecurityDog.sh

--- SecurityDog.sh
+++ SecurityDog.sh
@@ -453,14 +453,16 @@ sleep 5
 ## 11. Configura os arquivos Motd e Issue.net do banner ##
 EdiMotdIssue() {
 echo
-echo "Desabilitando mensagem de caixa de e-mail no login"
+echo "Desabilitando a mensagem de caixa de e-mail no login... "
 sed  -i "s/session    optional     pam_mail.so/#session    optional     pam_mail.so/" /etc/pam.d/sshd
 echo
-echo "Habilitando o issue.net em /etc/ssh/sshd_config"
+echo "Habilitando no arquivo /etc/ssh/sshd_config o issue.net... "
 sed -i 's/#Banner.*none/Banner \/etc\/issue.net/' /etc/ssh/sshd_config
+echo
+echo "Desabilitando no arquivo /etc/ssh/sshd_config a mensagem de último login... "
 sed -i 's/#PrintLastLog yes/PrintLastLog no/' /etc/ssh/sshd_config
 echo
-echo "Desabilitando antigos arquivos do motd"
+echo "Desabilitando antigos arquivos do motd... "
 chmod -x /etc/update-motd.d/10-uname
 mv /etc/update-motd.d/10-uname /etc/update-motd.d/10-uname.old
 mv /etc/motd /etc/motd.old
